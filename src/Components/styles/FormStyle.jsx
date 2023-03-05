8b7bfccfbf764b5c24c685b2e3c8b49d88c9b933
RadimYassin <radimyassin20@gamil.com>
2023-03-05 22:57:38 +0100

    create style form form and for home

---
+++ src/Components/styles/FormStyle.jsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+
+
+  export const FormConatiner=styled.form`
+    
+
+
+  
+    width:100%;
+    height:40px;
+    padding:3px;
+    display:flex;
+    flex-direction:row;
+    align-items:center;
+    
+  `
+
+export const InputType=styled.input`
+  font-family: 'Roboto', sans-serif;
+  color: #333;
+  font-size: 1.2rem;
+  border-radius: 0.2rem;
+  background-color: rgb(255, 255, 255);
+  border: none;
+
+
+
+padding:8px;
+ width:${(props)=>props.width ? props.width: "50% "};
+ &:focus{
+  outline:none;
+  border: 1px solid #0d253f9f;
+ }
+ 
+
+
+`
---
+++ src/Components/styles/homeStyle.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+
+
+  export const MainHome=styled.main`
+    
+
+
+
+    width:100%;
+   height:auto;
+
+  `
+
+  
+
+export const SeachHome=styled.div`
+    
+
+
+padding:8px 0;
+width:100%;
+ display:flex;
+ flex-direction:row;
+ align-items:center;
+ justify-content:space-between;
+`
+
