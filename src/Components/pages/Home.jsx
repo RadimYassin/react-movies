4f5dd4ccfdbeb4094dc4785c113f6f34bd28f34d
RadimYassin <radimyassin20@gamil.com>
2023-03-05 22:52:50 +0100

    updating in home

--- src/Components/pages/Home.jsx
+++ src/Components/pages/Home.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import { Container } from '../../GloblesStyles'
+import Form from '../Form/Form'
+import { MainHome ,SeachHome } from '../styles/homeStyle'
 
 export default function Home() {
   return (
-    <Container>Home</Container>
+    <Container bg mt="50px">
+          <MainHome>
+                <SeachHome>
+                      <Form>
+                        <Form.Input  type="text" placeholder="serch a movie"  />
+                      </Form>
+                      <h1>trend</h1>
+                </SeachHome>
+               
+          </MainHome>
+
+    </Container>
   )
 }
