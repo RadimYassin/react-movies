aebdecd419e55848564eb4bb08b6186442fb020d
RadimYassin <100171335+RadimYassin@users.noreply.github.com>
2023-03-05 01:07:10 +0100

    Update index.jsx

--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -1,1 +1,5 @@
 export {default as Navbar} from './Navbar/Navbar'
+export {default as Navbar} from './Navbar/Navbar'
+export {default as Home} from './pages/Home'
+export {default as Top} from './pages/Top'
+export {default as WatchList} from './pages/WatchList'
