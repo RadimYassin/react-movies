333662893090d8af97481fae0f00f6451f4d043e
RadimYassin <radimyassin20@gamil.com>
2023-03-06 01:33:58 +0100

    updating

--- src/Components/pages/Home.jsx
+++ src/Components/pages/Home.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect ,useState} from 'react'
 import { Container } from '../../GloblesStyles'
+import RegularList from '../../RegularList'
+import CardMovie from '../Container/CardMovie'
 import Form from '../Form/Form'
-import { MainHome ,SeachHome } from '../styles/homeStyle'
+import { MainHome ,SeachHome,MoviesHome } from '../styles/homeStyle'
 
 export default function Home() {
+  const [data, setData] = useState([]); // no data yet
+  const [isLoading, setIsLoading] = useState(false);
+
+
+  useEffect(() => {
+    setIsLoading(true);
+    axios.get("https://api.themoviedb.org/3/movie/popular?api_key=8bb482f2f727dd736af358131fd13dab&language=en-US&include_adult=false")
+      .then(data => {
+        setData(data.data.results); // update state with response
+      })
+      .catch(error => {
+        // handle any errors/rejected Promises
+        console.log(error.message);
+      })
+      .finally(() => setIsLoading(false)); // complete loading success/fail
+  }, []);
+  console.log(data);
   return (
     <Container bg mt="50px">
           <MainHome>
@@ -11,9 +31,21 @@ export default function Home() {
                       <Form>
                         <Form.Input  type="text" placeholder="serch a movie"  />
                       </Form>
-                      <h1>trend</h1>
+                       <select>
+                           <option value="">movie</option>
+                           <option value="">Peaple</option>
+                           <option value="">Tv Shows</option>
+                       </select>
                 </SeachHome>
+
+                 <MoviesHome>
+
+                      <RegularList items={data} ItemC={CardMovie} resourceName="movie"/>
+                
+                 </MoviesHome>
+                
                
+                
           </MainHome>
 
     </Container>
