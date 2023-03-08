885b7a7c0b4f59b0c8b40d641cc1c2f062e58d2e
RadimYassin <radimyassin20@gamil.com>
2023-03-08 01:46:09 +0100

    updating

--- src/Components/pages/Home.jsx
+++ src/Components/pages/Home.jsx
@@ -3,17 +3,27 @@ import React, { useEffect ,useState} from 'react'
 import { Container } from '../../GloblesStyles'
 import RegularList from '../../RegularList'
 import CardMovie from '../Container/CardMovie'
-import Form from '../Form/Form'
-import { MainHome ,SeachHome,MoviesHome,SelectHome } from '../styles/homeStyle'
+import Typography from '@mui/material/Typography';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+import { MainHome ,Box,MoviesHome,SelectHome,TextHome,PaginationHome } from '../styles/homeStyle'
 
 export default function Home() {
   const [data, setData] = useState([]); // no data yet
   const [isLoading, setIsLoading] = useState(false);
+  const [value,setValue]=useState("now_playing")
+  const [page, setPage] = React.useState(1);
 
 
+
+
+//"https://api.themoviedb.org/3/movie/now_playing?api_key=8bb482f2f727dd736af358131fd13dab&language=en-US&page=1
+//https://api.themoviedb.org/3/movie/popular?api_key=8bb482f2f727dd736af358131fd13dab&language=en-US&page=1
+//https://api.themoviedb.org/3/movie/top_rated?api_key=8bb482f2f727dd736af358131fd13dab&language=en-US&page=1
+//https://api.themoviedb.org/3/movie/upcoming?api_key=8bb482f2f727dd736af358131fd13dab&language=en-US&page=1
   useEffect(() => {
     setIsLoading(true);
-    axios.get("https://api.themoviedb.org/3/movie/popular?api_key=8bb482f2f727dd736af358131fd13dab&language=en-US&include_adult=false")
+    axios.get(`https://api.themoviedb.org/3/movie/${value}?api_key=8bb482f2f727dd736af358131fd13dab&language=en-US&page=${page}`)
       .then(data => {
         setData(data.data.results); // update state with response
       })
@@ -22,33 +32,40 @@ export default function Home() {
         console.log(error.message);
       })
       .finally(() => setIsLoading(false)); // complete loading success/fail
-  }, []);
+  }, [value,page]);
    // envent for select 
 
-   const SelectType=(e)=>{
-    console.log(e.target.value);
+
+   const SelectV=(e)=>{
+    setValue(e.target.value);
    }
+   const handleChange = (event, value) => {
+    setPage(value);
+  };
   return (
     <Container bg mt="50px">
           <MainHome>
-                <SeachHome>
-                      <Form>
-                        <Form.Input  type="text" placeholder="search a movies"  />
-                      </Form>
-                       <SelectHome onChange={SelectType} >
-                           <option value="movie">movie</option>
-                           <option value="peopale">Peaple</option>
-                           <option value="Tv Shows">Tv Shows</option>
+                <Box>
+                       <TextHome><p>{value && value}:</p></TextHome>
+                       <SelectHome onChange={SelectV} >
+                           <option value="now_playing">now_playing</option>
+                           <option value="popular">popular</option>
+                           <option value="top_rated">top_rated</option>
+                           <option value="upcoming">upcoming</option>
                        </SelectHome>
-                </SeachHome>
-
+                </Box>
+            
                  <MoviesHome>
-
+                      
                       <RegularList items={data} ItemC={CardMovie} resourceName="movie"/>
                 
                  </MoviesHome>
-                
-               
+                  <PaginationHome>
+                        <Stack spacing={2}>
+                    
+                        <Pagination count={10} page={page} onChange={handleChange} />
+                      </Stack>
+                  </PaginationHome>
                 
           </MainHome>
 
