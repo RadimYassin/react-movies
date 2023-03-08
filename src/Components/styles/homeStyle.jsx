927a1e7bcdab3431341ddd7c301450ca79555731
RadimYassin <radimyassin20@gamil.com>
2023-03-08 01:53:50 +0100

    updating

--- src/Components/styles/homeStyle.jsx
+++ src/Components/styles/homeStyle.jsx
@@ -20,10 +20,10 @@ width:100%;
  flex-direction:row;
  align-items:center;
  justify-content:space-between;
- margin-bottom:40px;
+ margin-bottom:60px;
+
+ 
 `
-
-
 
 export const MoviesHome=styled.div`
 
@@ -35,4 +35,46 @@ grid-template-columns: repeat(5, 1fr);
 grid-template-columns: repeat(2, 1fr);
     }
 
+`
+
+export const SelectHome=styled.select`
+appearance: none;
+  outline: 0;
+  border: 0;
+  box-shadow: none;
+  /* Personalize */
+  flex: 1;
+
+  padding: 0.5rem 1em;
+  border-radius:6px;
+
+  padding: 0.2rem 1em;
+  border-radius:6px;
+  color: #fff;
+  background-color:#0d253f;
+  background-image: none;
+  cursor: pointer;
+
+
+
+`
+
+
+
+export const TextHome=styled.div`
+width:85%;
+font-size:30px;
+@media screen and (max-width:960px){
+  font-size:20px;
+    }
+  
+`
+export const PaginationHome=styled.div`
+  
+
+
+  display:flex;
+  justify-content:center;
+  margin-top:20px;
+  margin-bottom:20px;
 `
